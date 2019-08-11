397cc8d5f8d9b8712150cb531d78f187f29f654a
chen-0825 <1132564240@qq.com>
2019-08-11 18:41:00 +0800

    修改

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,13 +1,9 @@
 $(function(){
 	var L1 = $("#L1,#L2,#L3,#L4,#L5,#L6,#L7");
-	// var L2 = $("#L1>i,#L2>i,#L3>i,#L4>i,#L5>i,#L6>i,#L7>i");
-	var index1;
-	L2.css({"display":"none"});
+	var index;
 	L1.click(function(){
-		index1 = L1.index(this);
+		index = L1.index(this);
 		L1.css({"background-color":""});
-		L1.eq(index1).css({"background-color":"rgb(237, 81, 80)"});
-		// L2.css({"display":"none"});
-		// L2.eq(index1).css({"display":"inline-block"});
+		L1.eq(index).css({"background-color":"rgb(237, 81, 80)"});
 	})
 })
