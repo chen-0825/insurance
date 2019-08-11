9d18c26f6f408d7f7e838c0ca2246f995685acc9
chen-0825 <1132564240@qq.com>
2019-08-11 18:35:40 +0800

    修改

--- src/main/java/com/java/controller/chy.java
+++ src/main/java/com/java/controller/chy.java
@@ -1,4 +1,13 @@
 package com.java.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class chy {
+
+    @RequestMapping("index")
+    public String index(){
+        return "backstage/index";
+    }
 }
