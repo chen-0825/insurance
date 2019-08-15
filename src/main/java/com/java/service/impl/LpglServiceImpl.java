5c5c25cfc940e55059088ff1645243424c05ebb5
zhouyang970925 <1499192230@qq.com>
2019-08-15 20:27:26 +0800

    2019/8/13

---
+++ src/main/java/com/java/service/impl/LpglServiceImpl.java
@@ -0,0 +1,45 @@
+package com.java.service.impl;
+
+import com.java.mapper.LpglMapper;
+import com.java.pojo.Lpgl;
+import com.java.service.LpglService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LpglServiceImpl implements LpglService {
+    @Autowired
+    private LpglMapper lpglMapper;
+
+    @Override
+    public int insertLpgl(Lpgl lpgl) {
+        return lpglMapper.insertLpgl(lpgl);
+    }
+
+    @Override
+    public int deleteLpgl(Integer id) {
+        return lpglMapper.deleteLpgl(id);
+    }
+
+    @Override
+    public int deleteLpgls(List<Integer> list) {
+        return lpglMapper.deleteLpgls(list);
+    }
+
+    @Override
+    public int updateLpgl(Lpgl lpgl) {
+        return lpglMapper.updateLpgl(lpgl);
+    }
+
+    @Override
+    public List<Lpgl> findAll() {
+        return lpglMapper.findAll();
+    }
+
+    @Override
+    public List<Lpgl> findBy(Lpgl lpgl) {
+        return lpglMapper.findBy(lpgl);
+    }
+}
