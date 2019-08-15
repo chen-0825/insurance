f3e7f884f71fc390803fc09c973a17f827035fb0
zhouyang970925 <1499192230@qq.com>
2019-08-15 09:42:17 +0800

    2019/8/13

--- src/main/java/com/java/mapper/LpglMapper.java
+++ src/main/java/com/java/mapper/LpglMapper.java
@@ -1,5 +1,19 @@
 package com.java.mapper;
 
-public interface LpglMapper {
+import com.java.pojo.Lpgl;
 
+import java.util.List;
+
+public interface LpglMapper {
+    //增加
+    int insertLpgl(Lpgl lpgl);
+    //删除
+    int deleteLpgl(Integer id);
+    //修改
+    int updateLpgl(Lpgl lpgl);
+
+    //查询所有
+    List<Lpgl> findAll();
+    //条件查询
+    List<Lpgl> findBy();
 }
